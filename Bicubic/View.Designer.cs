c4d527a3496ce2b4f6e046efc900eced9ed6224b
zirrusc <cloud.skyblue.0@gmail.com>
2013-01-05 00:20:44 +0900

    first commit

---
+++ Bicubic/View.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Bicubic
+{
+	partial class View
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.AutoScroll = true;
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(584, 362);
+			this.panel1.TabIndex = 1;
+			// 
+			// View
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 362);
+			this.Controls.Add(this.panel1);
+			this.Name = "View";
+			this.Text = "View";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ Bicubic/View.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bicubic
+{
+	public partial class View : Form
+	{
+		public View()
+		{
+			InitializeComponent();
+		}
+
+		public void SetImage(string path)
+		{
+			if (path == "")
+			{
+				pictureBox1.Image = null;
+			}
+			else
+			{
+				pictureBox1.Image = new Bitmap(path);
+
+			}
+		}
+
+		public void SetImage(Image i)
+		{
+			pictureBox1.Image = i;
+		}
+
+	}
+}
